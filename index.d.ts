ce2e7d20c9fa95677d7f2b4676e282c2623ec181
Keith Yeh <keithyehtw@gmail.com>
2019-09-27 23:22:14 +0800

    Add TypeScript type definitions #32 (#34)

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+type Options = {
+  custom?: string[] | { [key: string]: string; };
+  lang?: string;
+  maintainCase?: boolean;
+  replacement?: string;
+  separator?: string;
+  separateNumbers?: boolean;
+  tone?: boolean;
+} | string;
+
+declare function slug(text: string, opt?: Options): string;
+
+export = slug;
